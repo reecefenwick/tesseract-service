7c34f3f89c8d574a30978b4dc8502047dc963571
Reece F <Fenwickreece08@gmail.com>
2015-01-30 12:33:15 +1000

    Update jobs.js

--- app/models/jobs.js
+++ app/models/jobs.js
@@ -1,3 +1,11 @@
+/**
+ * jobs.js
+ *
+ * @description :: This is an object model of a job that is stored in the jobs database collection, this uses the mongoose
+ * library as an ORM.
+ * @docs        :: insert link to doco
+ */
+
 'use strict';
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
@@ -10,7 +18,8 @@ var jobSchema = new Schema({
     }],
     file_id: String,
     complete: { type: Boolean, default: false },
-    result: { type: String, default: null }
+    result: { type: String, default: null },
+    error: { type: String, default: null }
 });
 
 module.exports = mongoose.model('jobs', jobSchema);
