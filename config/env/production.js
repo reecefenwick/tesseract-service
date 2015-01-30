4b83b9b885b6a81af546b81d905c1235605489c1
Reece F <Fenwickreece08@gmail.com>
2015-01-30 12:28:40 +1000

    Update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -1,3 +1,22 @@
 /**
- * Created by reecefenwick on 20/01/15.
+ * production.js
+ *
+ * @description :: This is the configuration file for the servers with NODE_ENV of 'production'
+ * @docs        :: http://sailsjs.org/#!documentation/models
  */
+module.exports = {
+    mq: {
+        host: 'amqp://wva51351',
+        channel: 'OCR',
+        options: {
+            durable: false,
+            noAck: false,
+            prefetch: 2
+        }
+    },
+    db: {
+        host: 'mongodb://wva51351',
+        port: 27017,
+        name: 'ocr'
+    }
+};
